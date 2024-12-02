d97655cf1306d45d477464c0aced3c0985a19bd2
Blake Williams <blake@blakewilliams.me>
2024-12-02 18:39:23 -0500

    Day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -16,14 +17,14 @@ type Report struct {
 
 func NewReport(line string) Report {
 	rawLevels := strings.Split(line, " ")
-	levels := make([]int, len(rawLevels))
+	levels := make([]int, 0)
 
-	for i, rawLevel := range rawLevels {
+	for _, rawLevel := range rawLevels {
 		level, err := strconv.Atoi(rawLevel)
 		if err != nil {
 			panic(err)
 		}
-		levels[i] = level
+		levels = append(levels, level)
 	}
 
 	return Report{
@@ -32,16 +33,44 @@ func NewReport(line string) Report {
 }
 
 func (r *Report) Safe() bool {
-	last := r.levels[0]
-	desc := r.levels[0] > r.levels[1]
+	return checkLevels(r.levels, false)
+}
 
-	for _, level := range r.levels[1:] {
+func (r *Report) MostlySafe() bool {
+	return checkLevels(r.levels, true)
+}
+
+func checkLevels(levels []int, singleException bool) bool {
+	last := levels[0]
+	desc := levels[0] > levels[1]
+
+	for i, level := range levels[1:] {
 		diff := last - level
+		safe := true
+
 		if desc && (diff < 1 || diff > 3) {
-			return false
+			safe = false
 		} else if !desc && (diff > -1 || diff < -3) {
+			safe = false
+		}
+
+		if !safe && singleException {
+			n := i + 1
+
+			pre := slices.Delete(slices.Clone(levels), n, n+1)
+			post := slices.Delete(slices.Clone(levels), n-1, n)
+			firstRes := false
+
+			// edge case: handle removing the first element
+			if n == 2 {
+				firstRes = checkLevels(slices.Delete(slices.Clone(levels), 0, 1), false)
+			}
+
+			return firstRes || checkLevels(pre, false) || checkLevels(post, false)
+		} else if !safe {
 			return false
 		}
+
 		last = level
 	}
 
@@ -61,4 +90,15 @@ func main() {
 	}
 
 	fmt.Printf("Safe reports: %d\n", safe)
+
+	safe = 0
+	for _, line := range reports {
+		report := NewReport(line)
+
+		if report.MostlySafe() {
+			safe++
+		}
+	}
+
+	fmt.Printf("Safe reports with 1 exception: %d\n", safe)
 }
