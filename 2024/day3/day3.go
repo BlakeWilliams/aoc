e0553560696544557e225e8ee2309efb84fc0f80
Blake Williams <blake@blakewilliams.me>
2024-12-03 20:15:43 -0500

    Add part 2

--- 2024/day3/day3.go
+++ 2024/day3/day3.go
@@ -8,18 +8,26 @@ import (
 	"strings"
 )
 
-var mulRegex = regexp.MustCompile(`mul\((\d+,\d+)\)`)
+var mulRegex = regexp.MustCompile(`mul\((\d+,\d+)\)|don't\(\)|do\(\)`)
 
 //go:embed input
 var input string
 
 func main() {
+	part1()
+	part2()
+}
+
+func part1() {
 	total := 0
 
 	muls := mulRegex.FindAllStringSubmatch(input, -1)
 	for _, mul := range muls {
+		if mul[0] == "do()" || mul[0] == "don't()" {
+			continue
+		}
+
 		nums := strings.Split(mul[1], ",")
-		fmt.Println(nums[1])
 		first, err := strconv.Atoi(nums[0])
 		if err != nil {
 			panic(err)
@@ -32,5 +40,39 @@ func main() {
 		total += first * second
 	}
 
-	fmt.Printf("Total %d\n", total)
+	fmt.Printf("Total: %d\n", total)
+}
+
+func part2() {
+	total := 0
+	muls := mulRegex.FindAllStringSubmatch(input, -1)
+	enabled := true
+
+	for _, mul := range muls {
+		if mul[0] == "don't()" {
+			enabled = false
+			continue
+		} else if mul[0] == "do()" {
+			enabled = true
+			continue
+		}
+
+		if !enabled {
+			continue
+		}
+
+		nums := strings.Split(mul[1], ",")
+		first, err := strconv.Atoi(nums[0])
+		if err != nil {
+			panic(err)
+		}
+		second, err := strconv.Atoi(nums[1])
+		if err != nil {
+			panic(err)
+		}
+
+		total += first * second
+	}
+
+	fmt.Printf("Total with toggle: %d\n", total)
 }
